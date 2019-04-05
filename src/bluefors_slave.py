ddc5a61523909faaf1d0b2793bdd43519e641d70
Gleb Fedorov <vdrhtc@gmail.com>
2019-04-05 23:41:04 +0300

    Slave interface adopted

--- src/bluefors_slave.py
+++ src/bluefors_slave.py
@@ -1,94 +1,16 @@
 import csv
 import os
 import re
-import socket
-import urllib
 from datetime import datetime
-from threading import Thread
-from time import sleep
-import ssl
-
 from numpy import reshape, log10
+from slave import Slave
 
-from src.LoggingServer import LoggingServer
 
-
-class BlueforsClient:
+class BlueforsSlave(Slave):
 
     def __init__(self, nickname, password, server_address, server_port, logs_path):
-
-        self._server_address = server_address
-        self._password = password
-        self._server_port = server_port
         self._logs_path = logs_path
-        self._nickname = nickname
-        self._logger = LoggingServer.getInstance()
-
-        self._context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-        self._context.verify_mode = ssl.CERT_REQUIRED
-        certurl = "https://raw.githubusercontent.com/vdrhtc/overseer/master/domain.crt"
-        self._certfile = urllib.request.urlretrieve(certurl)[0]
-        self._context.load_verify_locations(self._certfile)
-
-        self._secure_socket = self._context.wrap_socket(socket.socket())  # instantiate
-
-        self._secure_socket.connect((server_address, server_port))  # connect to the server
-
-        self._stop = False
-        self._updater = Thread(target=self._act)
-        self._updater.setDaemon(True)
-
-        self._strategies = {"update": self._send_update,
-                            "reconnect": self._reconnect,
-                            "handshake": self._handshake}
-        self._current_strategy = "handshake"
-
-    def launch(self):
-        self._stop = False
-        self._updater.start()
-
-    def _act(self):
-        while not self._stop:
-            try:
-                self._strategies[self._current_strategy]()
-            except (TimeoutError, ConnectionRefusedError, ConnectionResetError, ConnectionAbortedError, ConnectionError) as e:
-                self._logger.warn(str(e))
-                sleep(15)
-                self._current_strategy = "reconnect"
-            except Exception as e:
-                self._logger.warn(str(e))
-                print(e)
-                break
-        self._secure_socket.close()
-
-    def _send_update(self):
-        print("\rSending update, " + str(datetime.now()), end="")
-        try:
-            data = self.generate_info_message().encode()
-        except Exception as e:
-            data = str(e).encode()
-
-        self._secure_socket.send(data)
-        sleep(15)
-
-    def _reconnect(self):
-        print("\rReconnecting...", end="")
-
-        self._secure_socket.close()
-        self._secure_socket = self._context.wrap_socket(socket.socket())
-        self._secure_socket.connect((self._server_address, self._server_port))  # connect to the server
-        self._current_strategy = "handshake"
-
-    def _handshake(self):
-        self._secure_socket.send((self._nickname+"\r\n"+self._password).encode())
-        response = self._secure_socket.recv(1024).decode()
-        if response == self._nickname:
-            self._current_strategy = "update"
-            print("Successful handshake!")
-        else:
-            print(" "+response, end="")
-            self._current_strategy = "reconnect"
-            sleep(15)
+        super().__init__(nickname, password, server_address, server_port)
 
     def generate_info_message(self):
         on_off = {"0": "⚪️", "1": "🔵", "2": '🌕'}
