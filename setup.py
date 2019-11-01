6cf5a7632424bb2616ab1bfd0a95c43f3ac2f033
GrigorevSasha <grigorev.sasha8008@gmail.com>
2019-11-01 17:16:49 +0300

    Added setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="bluefors_slave",
+    py_modules=["bluefors_slave"],
+    install_requires=["overseer_slave","loggingserver"],
+    version="1.0b2",
+    author="Gleb Fedorov",
+    author_email="vdrhtc@gmail.com",
+    description="Bluefors interface for the Overseer",
+    long_description='''This package is to monitor the state of Bluefors fridges''',
+    long_description_content_type="text/markdown",
+    url="https://github.com/vdrhtc/bluefors-monitor",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
